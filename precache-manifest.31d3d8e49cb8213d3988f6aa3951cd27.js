f15db47f83f1ff4e9e67517624c382636897f736
Siddharth <mishrasiddharth1507@gmail.com>
2020-07-25 00:36:26 +0530

    Updates

---
+++ precache-manifest.31d3d8e49cb8213d3988f6aa3951cd27.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157babf13636d7698ce6403e7ecaa4ef",
+    "url": "/rentkar/index.html"
+  },
+  {
+    "revision": "bb7b28b3c9fdce809c69",
+    "url": "/rentkar/static/css/2.740d69ab.chunk.css"
+  },
+  {
+    "revision": "d791d2eb6151cbddf92e",
+    "url": "/rentkar/static/css/main.010b0637.chunk.css"
+  },
+  {
+    "revision": "bb7b28b3c9fdce809c69",
+    "url": "/rentkar/static/js/2.2890a3df.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/rentkar/static/js/2.2890a3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d791d2eb6151cbddf92e",
+    "url": "/rentkar/static/js/main.db0d7806.chunk.js"
+  },
+  {
+    "revision": "16cac70053133ebf7232",
+    "url": "/rentkar/static/js/runtime-main.c01f0d97.js"
+  },
+  {
+    "revision": "822137416ed223d3e977f67dcd0e8112",
+    "url": "/rentkar/static/media/changes_icon-01.82213741.png"
+  },
+  {
+    "revision": "5d492adbc491b9ef26b6bcfc525c177e",
+    "url": "/rentkar/static/media/changes_icon-03.5d492adb.png"
+  },
+  {
+    "revision": "7f6216560d825dfd039bc51b626fd05a",
+    "url": "/rentkar/static/media/covid sefty web-03.7f621656.png"
+  },
+  {
+    "revision": "61c8a755ba47d93ef91174abb37bd500",
+    "url": "/rentkar/static/media/covid sefty web-04.61c8a755.png"
+  },
+  {
+    "revision": "da6cce61228a205b22841de8504ef0ff",
+    "url": "/rentkar/static/media/covid.da6cce61.png"
+  },
+  {
+    "revision": "0dea09a40e34635251376313a7cce9d9",
+    "url": "/rentkar/static/media/cust1.0dea09a4.png"
+  },
+  {
+    "revision": "4337d0638c15aeca32372c864b8a1459",
+    "url": "/rentkar/static/media/cust2.4337d063.png"
+  },
+  {
+    "revision": "4fddd023912d6fc81a8bf0cb040127ce",
+    "url": "/rentkar/static/media/gaming.4fddd023.jpg"
+  },
+  {
+    "revision": "c56564eda7fdba5ee2c442225bf5dd6a",
+    "url": "/rentkar/static/media/guitar.c56564ed.png"
+  },
+  {
+    "revision": "7d3d55f95d0cf5e4e4c6f3f5376596ea",
+    "url": "/rentkar/static/media/logo.7d3d55f9.png"
+  },
+  {
+    "revision": "574b44e9bd91b07e4ed384233748d8bd",
+    "url": "/rentkar/static/media/musical.574b44e9.jpg"
+  },
+  {
+    "revision": "a4f0995223c763ed05e6fc09a99bf5e7",
+    "url": "/rentkar/static/media/share.a4f09952.png"
+  },
+  {
+    "revision": "44b2f1a1e674676aebbc5fbeab88fdd1",
+    "url": "/rentkar/static/media/slide1.44b2f1a1.jpg"
+  },
+  {
+    "revision": "aa9395f25ae4643f55f6c67a637050c7",
+    "url": "/rentkar/static/media/slide2.aa9395f2.jpg"
+  },
+  {
+    "revision": "e17bb92254323d437aa5ab429df2287c",
+    "url": "/rentkar/static/media/slide3.e17bb922.jpg"
+  },
+  {
+    "revision": "e6fcd0e0fca89bd606ced01eefbcca5a",
+    "url": "/rentkar/static/media/wave.e6fcd0e0.png"
+  }
+]);
