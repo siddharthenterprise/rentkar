2058bd995f05caf26b22bc11ffb3a56805317fcf
Siddharth <mishrasiddharth1507@gmail.com>
2020-08-03 17:55:44 +0530

    interaction

--- src/components/List_item/Items.js
+++ src/components/List_item/Items.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react'
-import guitar from './guitar.png';
+import { guitar } from './guitar.png';
 
 import './Item.css';
 
 export class Item extends Component {
+
     image = {
         height: '120px',
         width: '100px',
-        borderRadius: '5px'
+        borderRadius: '5px',
+        // backgroundImage: 'url(' + guitar + ')'
 
     }
+
+    selected = [];
+
+    // itemstyle = {
+    //     margin: '15px',
+    //     borderRadius: '5px',
+    //     boxShadow: '3px 3px 10px rgba(0, 0, 0, 0.2)',
+    //     backgroundImage: 'url(' + {guitar} + ')'
+    // }
     items = [
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>,
-        <img style={this.image} src={guitar}></img>
+        <div className='abc' style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>,
+        <div style={this.image} ></div>
     ]
+
+
+
     continue = e => {
         e.preventDefault();
         this.props.nextStep();
@@ -42,6 +52,18 @@ export class Item extends Component {
         e.preventDefault();
         this.props.prevStep();
     };
+    changeStyle = (e, data) => {
+        if (!this.selected.includes(data)) {
+            e.target.style.boxShadow = `inset 0 0 0 1000px rgba(143,227,205,0.9)`;
+            this.selected.push(data)
+        }
+        else {
+            e.target.style.boxShadow = `inset 0 0 0 1000px rgba(143,227,205,0)`;
+            this.selected.pop(data);
+        }
+        console.log(this.selected);
+        // alert(e.target.style.borderRadius = `2px`);
+    }
     render() {
         return (
             <div className='chooseitems'>
@@ -50,8 +72,9 @@ export class Item extends Component {
                 <div className='items'>
                     {
                         this.items.map((item, index) => {
+                            console.log(index);
                             return (
-                                <div className='item'>{item}</div>
+                                <div className='item' data={index} onClick={((e) => this.changeStyle(e, index))}>{item}</div>
                             )
                         })
                     }
