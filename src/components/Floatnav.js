2ffa9a326f3c301ca8ec2ab88ea74e2c864756e6
Siddharth <mishrasiddharth1507@gmail.com>
2020-08-05 20:04:18 +0530

    mobile

--- src/components/Floatnav.js
+++ src/components/Floatnav.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { about } from '../images/about.png';
+import { search } from '../images/search.png';
 import './Float.css';
 
 export class Floatnav extends Component {
     render() {
         return (
             <div className="floatnav">
-                <div class="mtop"><nav class="fixed-bottom text-center dark customnav"><div class="d-flex justify-content-between"><div class="container"><i class="fas fa-home fa-2x fa-gradient"></i></div><div class="container"><i class="fas fa-search fa-2x"></i></div><div class="container"><i class="fas fa-shopping-bag fa-2x"></i></div><div class="container"><i class="fas fa-user fa-2x"></i></div></div></nav></div>
+                <div class="mtop">
+                    <nav class="fixed-bottom text-center dark customnav">
+                        <div class="d-flex justify-content-between">
+                            <div class="container">
+                                <i class="fas fa-home fa-2x fa-gradient"></i>
+                            </div>
+                            <div class="container">
+                                <i class="fas fa-search fa-2x fa-gradient"></i>
+                            </div>
+                            <div class="container">
+                                <i class="fas fa-shopping-bag fa-2x fa-gradient"></i>
+                            </div>
+                            <div class="container">
+                                <i class="fas fa-user fa-2x fa-gradient"></i>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
             </div>
         )
     }
