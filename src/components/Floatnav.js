a631899a2d53d91324f908be01cd44b4d3f39bb6
Siddharth <mishrasiddharth1507@gmail.com>
2020-08-13 21:24:47 +0530

    float_bar2

--- src/components/Floatnav.js
+++ src/components/Floatnav.js
@@ -7,24 +7,56 @@ import { home } from '../images/about.png';
 import './Float.css';
 
 export class Floatnav extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            home: require('../images/home.png'),
+            search: require('../images/search.png'),
+            bag: require('../images/bagb.png'),
+            about: require('../images/about.png')
+        }
+    }
+
+    onHome = (e) => {
+        this.setState({ home: require('../images/home.png') });
+        this.setState({ bag: require('../images/bagb.png') });
+        // this.setState({ about: require('../images/bagb.png') });
+
+    }
+    onSearch = (e) => {
+        this.setState({ home: require('../images/homeo.png') });
+        this.setState({ bag: require('../images/bagb.png') });
+
+    }
+    onBag = (e) => {
+        this.setState({ home: require('../images/homeo.png') });
+        this.setState({ bag: require('../images/bag.png') });
+    }
+    onAbout = (e) => {
+        this.setState({ home: require('../images/homeo.png') });
+        this.setState({ bag: require('../images/bagb.png') });
+    }
+
+
     render() {
         return (
             <div className="floatnav">
                 <div class="mtop">
                     <nav class="fixed-bottom text-center dark customnav">
                         <div class="d-flex justify-content-between">
-                            <div class="container" id="home_float">
-                                <img src={require('../images/home.png')} className="img-thumbnail" />
+                            <div class="container" id="home_float" onClick={this.onHome}>
+                                <img src={this.state.home} className="img-thumbnail" />
                             </div>
-                            <div class="container">
+                            <div class="container" onClick={this.onSearch}>
 
-                                <img src={require('../images/search.png')} className="img-thumbnail" />
+                                <img src={this.state.search} className="img-thumbnail" />
                             </div>
-                            <div class="container">
-                                <img src={require('../images/bagb.png')} className="img-thumbnail" />
+                            <div class="container" onClick={this.onBag}>
+                                <img src={this.state.bag} className="img-thumbnail" />
                             </div>
-                            <div class="container">
-                                <img src={require('../images/about.png')} className="img-thumbnail" />
+                            <div class="container" onClick={this.onAbout}>
+                                <img src={this.state.about} className="img-thumbnail" />
                             </div>
                         </div>
                     </nav>
