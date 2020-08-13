9338f0fdf9c91c822e857189198aec7b7db49059
Siddharth <mishrasiddharth1507@gmail.com>
2020-08-13 20:34:34 +0530

    float_bar

--- src/components/Floatnav.js
+++ src/components/Floatnav.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { about } from '../images/about.png';
 import { search } from '../images/search.png';
+import { home } from '../images/about.png';
+
+
 import './Float.css';
 
 export class Floatnav extends Component {
@@ -10,17 +13,18 @@ export class Floatnav extends Component {
                 <div class="mtop">
                     <nav class="fixed-bottom text-center dark customnav">
                         <div class="d-flex justify-content-between">
-                            <div class="container">
-                                <i class="fas fa-home fa-2x fa-gradient"></i>
+                            <div class="container" id="home_float">
+                                <img src={require('../images/home.png')} className="img-thumbnail" />
                             </div>
                             <div class="container">
-                                <i class="fas fa-search fa-2x fa-gradient"></i>
+
+                                <img src={require('../images/search.png')} className="img-thumbnail" />
                             </div>
                             <div class="container">
-                                <i class="fas fa-shopping-bag fa-2x fa-gradient"></i>
+                                <img src={require('../images/bagb.png')} className="img-thumbnail" />
                             </div>
                             <div class="container">
-                                <i class="fas fa-user fa-2x fa-gradient"></i>
+                                <img src={require('../images/about.png')} className="img-thumbnail" />
                             </div>
                         </div>
                     </nav>
