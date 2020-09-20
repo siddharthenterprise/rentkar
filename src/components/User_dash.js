6c365a3512e2a336d6fb0abfc9b3005c5e8442e0
Siddharth <mishrasiddharth1507@gmail.com>
2020-09-20 23:17:10 +0530

    my_rental

--- src/components/User_dash.js
+++ src/components/User_dash.js
@@ -15,6 +15,9 @@ import support_g from '../images/icons/support_g.png';
 import settings_g from '../images/icons/settings_g.png';
 
 import pencil from '../images/icons/pencil_white.png';
+import pencil_b from '../images/icons/pencil.png';
+import guitar from "../images/guitar2.png";
+
 
 import './user_dash.css';
 import { Card, Image } from "semantic-ui-react";
@@ -23,6 +26,94 @@ const icons = [rental_s, listing_s, shareCredit_s, verification_s, support_s, se
 const icons_g = [rental_g, listing_g, shareCredit_g, verification_g, support_g, settings_g]
 const iconDesc = ['My Rentals', 'My Listings', 'Share Credits', 'Verification', 'Help & Support', 'Settings']
 
+class My_bag extends Component {
+    render() {
+        const Order_card = () => (
+            <div class="image_detail">
+                <div class="image">
+                    <img src={guitar} alt="product" />
+                </div>
+                <div class="detail">
+                    <div class="name">
+                        <div class="main_head">
+                            <div>
+                                <h4 style={{ 'fontWeight': '700' }}>Fender Bullet Stratocaster</h4>
+                            </div>
+                            <div>
+                                <h6 style={{ 'color': 'red' }}>Hurry? Only few in stock</h6>
+                            </div>
+                        </div>
+                        <div class="edit_order">
+                            <h6><a>Edit Order<img src={pencil_b}></img></a><span> | </span><a style={{ 'color': 'red' }}>Remove<i class="fas fa-trash-alt"></i></a></h6>
+                        </div>
+                    </div>
+                    <div class="price">
+                        <div class="rent">
+                            <div className="name__">
+                                <h6 >Rent</h6>
+                            </div>
+                            <div>
+                                <h4><i class="fas fa-rupee-sign    "></i>190 /Day</h4>
+                            </div>
+                        </div>
+                        <div class="tenure">
+                            <div className="name__">
+                                <h6>Tenure <i class="far fa-clock"></i></h6>
+                            </div>
+                            <div>
+                                <h4>2 Weeks</h4>
+                            </div>
+                        </div>
+                        <div class="deposit">
+                            <div className="name__">
+                                <h6>Deposit</h6>
+                            </div>
+
+                            <h4>0</h4>
+                        </div>
+
+                    </div>
+                    <div class="total">
+                        <div class="mybag_button">
+                            <div class="placeorder">
+                                <button className="but1">Place Order</button>
+                            </div>
+                            <div class="saveforlater">
+                                <button className="but2">Save for Later</button>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="total_price">
+                                <h4>Total: <i class="fas fa-rupee-sign    "></i> 2660</h4>
+                                <h6><i class="fas fa-exclamation-circle"></i>Price Breakdown</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        )
+        return (
+            <div>
+                <div className="order_card">
+                    <Order_card />
+
+                </div>
+                <div className="order_card">
+                    <Order_card />
+                </div>
+                <div class="checkout">
+                    <div class="cost">
+                        <h6>Total:  &nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-rupee-sign    "></i>5460 &nbsp;&nbsp;|&nbsp;&nbsp; <i class="fa fa-truck" aria-hidden="true"></i> Delivery by 21 September</h6>
+                    </div>
+                    <div class="checkout_button">
+                        <button>Place Order</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 
 export class User_dash extends Component {
@@ -98,6 +189,9 @@ export class User_dash extends Component {
                         <a href='#'>Privacy Policy</a>
                         <a href='#'>Privacy Policy</a>
                     </div>
+                </div>
+                <div class="right_screen">
+                    <My_bag />
                 </div>
             </div>
         )
